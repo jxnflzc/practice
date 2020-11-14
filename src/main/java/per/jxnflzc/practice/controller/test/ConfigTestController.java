aa46063ab39ec798194a064a2ad07805818db2be
Jxnflzc <245186727@qq.com>
2020-11-14 23:44:42 +0800

    去除冗余import

--- src/main/java/per/jxnflzc/practice/controller/test/ConfigTestController.java
+++ src/main/java/per/jxnflzc/practice/controller/test/ConfigTestController.java
@@ -4,13 +4,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import per.jxnflzc.practice.model.ResponseBodyInfo;
-import per.jxnflzc.practice.model.test.TestInfo;
 
 @Api(tags = {"配置功能测试接口"})
 @RestController
