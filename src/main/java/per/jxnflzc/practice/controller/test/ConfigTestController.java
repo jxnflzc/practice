1de6e72ec8a473fb556b52a705b87137137908ab
Jxnflzc <245186727@qq.com>
2020-11-15 00:40:37 +0800

    添加配置文件下载

--- src/main/java/per/jxnflzc/practice/controller/test/ConfigTestController.java
+++ src/main/java/per/jxnflzc/practice/controller/test/ConfigTestController.java
@@ -10,11 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import per.jxnflzc.practice.model.ResponseBodyInfo;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Api(tags = {"配置功能测试接口"})
 @RestController
 @RequestMapping("/test/v1/conf")
 public class ConfigTestController {
     private static final Logger logger = LoggerFactory.getLogger(ConfigTestController.class);
+
+    private static final String PATH_PRE = "/properties/";
+
+    private static final String CLASS_PATH_PRE = "classpath:properties/";
 
     @Value("${test.hello:Hello}")
     private String hello;
@@ -23,5 +32,48 @@ public class ConfigTestController {
     @GetMapping(value = "/hello")
     public ResponseBodyInfo hello() {
         return ResponseBodyInfo.success(hello);
+    }
+
+    @ApiOperation(value = "获取配置文件")
+    @GetMapping(value = "/file")
+    public ResponseBodyInfo file(HttpServletResponse response) {
+        String filename = "test.properties";
+        String filepath = PATH_PRE + filename;
+        byte[] buffer = new byte[1024];
+        InputStream fis = null;
+        BufferedInputStream bis = null;
+        try {
+            response.setHeader("content-type", "application/octet-stream");
+            response.setContentType("application/octet-stream");
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, StandardCharsets.UTF_8));
+
+            fis = this.getClass().getResourceAsStream(filepath);
+            bis = new BufferedInputStream(fis);
+            OutputStream os = response.getOutputStream();
+            int i = bis.read(buffer);
+            while (i != -1) {
+                os.write(buffer, 0, i);
+                i = bis.read(buffer);
+            }
+        } catch (Exception e) {
+            logger.debug("下载失败:{}", e.toString());
+            return ResponseBodyInfo.success("下载失败");
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return ResponseBodyInfo.success("下载成功");
     }
 }
