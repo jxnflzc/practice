38a5a4723358060df0421ee1b134ce8a45b19424
Jxnflzc <245186727@qq.com>
2020-11-21 22:54:28 +0800

    0.3.1
    添加登录拦截器，防止用户多次登录后产生冗余token

---
+++ src/main/java/per/jxnflzc/practice/interceptor/LoginInterceptor.java
@@ -0,0 +1,97 @@
+package per.jxnflzc.practice.interceptor;
+
+import com.alibaba.fastjson.JSON;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import per.jxnflzc.practice.anno.NeedLogin;
+import per.jxnflzc.practice.model.CurrentUser;
+import per.jxnflzc.practice.model.ResponseBodyInfo;
+import per.jxnflzc.practice.model.enums.ResponseCode;
+import per.jxnflzc.practice.util.RedisUtil;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+
+public class AuthorizationInterceptor implements HandlerInterceptor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizationInterceptor.class);
+
+    private RedisUtil redisUtil;
+
+    @Autowired
+    public void setRedisUtil(RedisUtil redisUtil) {
+        this.redisUtil = redisUtil;
+    }
+
+    //存放鉴权信息的Header名称，默认是Authorization
+    private String httpHeaderName = "Authorization";
+
+    //鉴权失败后返回的错误信息，默认为401 unauthorized
+    private final String unauthorizedErrorMessage = "401 unauthorized";
+
+    //鉴权失败后返回的HTTP错误码，默认为401
+    private final int unauthorizedErrorCode = HttpServletResponse.SC_UNAUTHORIZED;
+
+    //存放登录用户模型Key的Request Key
+    public static final String REQUEST_CURRENT_KEY = "REQUEST_CURRENT_KEY";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Method method = handlerMethod.getMethod();
+
+        // 如果打上了AuthToken注解则需要验证token
+        if (method.isAnnotationPresent(NeedLogin.class)) {
+            NeedLogin needLogin = method.getAnnotation(NeedLogin.class);
+            if (needLogin.required()) {
+                // 从HTTP请求头中获取TOKEN信息
+                String token = request.getHeader(httpHeaderName);
+
+                if (!StringUtils.hasText(token) || !redisUtil.hasKey(token)) {
+                    returnInfo(response);
+                    return false;
+                } else {
+                    CurrentUser currentUser = redisUtil.getCurrentUser(token);
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(currentUser,null);
+                    authentication.setDetails(request);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+
+    private void returnInfo(HttpServletResponse response){
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            ResponseBodyInfo result = ResponseBodyInfo.build(ResponseCode.NOT_LOGIN);
+            writer.print(JSON.toJSONString(result));
+        } catch (IOException e) {
+            LOGGER.error("拦截器输出流异常:{}", e.toString());
+        }
+    }
+}
